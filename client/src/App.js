d7e38ddef7528259ba099a908475c20360947537
Campbell Conyers <campbell.conyers@appscore.net>
2023-03-21 22:27:32 +0000

    useAsync hook

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import { PostList } from "./components/PostLists";
 
 function App() {
-  return <Routes>
-    <Route path="/" element={<PostList />} />
-    <Route path="/posts/:id" element={<h1>POST</h1>} />
-  </Routes>
+  return (
+    <div className='container'>
+      <Routes>
+        <Route path="/" element={<PostList />} />
+        <Route path="/posts/:id" element={<h1>POST</h1>} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App;
