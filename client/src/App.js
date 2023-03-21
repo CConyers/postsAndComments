dba969d8524bf356e7583e63289aecc461286e27
Campbell Conyers <campbell.conyers@appscore.net>
2023-03-21 22:05:35 +0000

    added react router dom and postList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import { Routes, Route } from 'react-router-dom';
+import { PostList } from "./components/PostLists";
 
 function App() {
-  return (
-    <h1>Hello World</h1>
-  );
+  return <Routes>
+    <Route path="/" element={<PostList />} />
+    <Route path="/posts/:id" element={<h1>POST</h1>} />
+  </Routes>
 }
 
 export default App;
